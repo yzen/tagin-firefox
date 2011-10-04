e60e08ed0a0a774469b6181f4c5612b15e6a3f00
Yura Zenevich <yura.zenevich@gmail.com>
2011-10-04 01:06:18 -0400

    Linted all js files.

--- data/taginUI.js
+++ data/taginUI.js
@@ -1,32 +1,36 @@
+/*global jQuery, self*/
+
 (function ($) {
 
+    "use strict";
+
     var tagin = tagin || {};
-    
+
     var toArray = function (data) {
         var key, arr = [];
         for (key in data) {
-            arr.push({
-                tag: key,
-                distance: data[key]
-            });
+            if (data.hasOwnProperty(key)) {
+                arr.push({
+                    tag: key,
+                    distance: data[key]
+                });
+            }
         }
         return arr.sort(function (a, b) {
             return a.distance - b.distance;
         });
     };
-    
+
     var renderTags = function (that, data) {
         $(".currentTag").detach();
-        var tagArray = toArray(data), i;
-        for (i = 0; i < tagArray.length; ++i) {
-            var tag = tagArray[i];
-            var currentTag = $("<div></div>").addClass("currentTag")
-                                             .text(tag.tag)
-                                             .css("font-size", (0.4 / tag.distance + 0.4) + "em");
-            that.tags.append(currentTag);
-        }
+        $.each(toArray(data), function (i, tag) {
+            that.tags.append($("<div></div>")
+                .addClass("currentTag")
+                .text(tag.tag)
+                .css("font-size", (0.4 / tag.distance + 0.4) + "em"));
+        });
     };
-    
+
     var bindEvents = function (that) {
         that.button.click(function () {
             self.port.emit("tagSubmitted", that.tag.val());
@@ -35,20 +39,20 @@
             renderTags(that, data);
         });
     };
-    
+
     var setup = function (that) {
         that.button = $(".submitTag");
         that.tag = $(".tag");
         that.tags = $(".tags");
     };
-    
+
     tagin.main = function () {
         var that = {};
         setup(that);
         bindEvents(that);
         return that;
     };
-    
+
     tagin.main();
-    
+
 })(jQuery);
